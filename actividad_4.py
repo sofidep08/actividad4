d32f6f48c974fec208c50b095e901ebe9edae113
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-07-09 11:06:21 -0600

    opciones de menu

--- actividad_4.py
+++ actividad_4.py
@@ -6,8 +6,29 @@ class Farmacia:
     def agregar_medicamento (self,nombre_medicamento,dosis):
         pila.append((nombre_medicamento,dosis))
         print("Medicamento guardado")
-    def entregar_medicamento (self,nombre_medicamento,dosis):
+    def entregar_medicamento (self):
         pila.pop(0)
         print("Medicamento entregado")
-    def mostrar_pila(self,nombre_medicamento,dosis):
-        print("Medicamentos guardados", pila)
+    def mostrar_pila(self):
+        print("Medicamentos guardados")
+        print(pila)
+
+    opcion=0
+    while opcion != 7:
+        print("[4] Agregar medicamentos")
+        print("[5] Agregar medicamentos")
+        print("[6] Agregar medicamentos")
+        print("[7] Agregar medicamentos")
+        opcion = input(print("Elija una opcion"))
+
+        if opcion == "4":
+            medicamento = input("Ingrese el nombre del medicamento: ")
+            dosiss = input("Ingrese la dosis del medicamento: ")
+            agregar_medicamento(medicamento,dosiss)
+        elif opcion == "5":
+            print("Entregando medicamento.....")
+            print()
+            entregar_medicamento()
+        elif opcion == "6":
+            print("Mostrando elementos en la pila")
+            mostrar_pila()
