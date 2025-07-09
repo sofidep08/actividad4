bafff3d1ad541cd11649b90e638f8fc8d3acab7d
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-07-09 10:47:06 -0600

    clase farmacia y agregar medicamento

--- actividad_4.py
+++ actividad_4.py
@@ -1,1 +1,8 @@
-print("f")
+pila = []
+class Farmacia:
+    def __init__(self,nombre_medicamentos,dosis):
+        self.nombre_medicamento = nombre_medicamentos
+        self.dosis = dosis
+    def agregar_medicamento (self,nombre_medicamento,dosis):
+        pila.append((nombre_medicamento,dosis))
+        print("Medicamento guardado")
